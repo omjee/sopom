fc21b36d63930caf84a938fd9b83034f16fba561
Thibault <thhumbert@gmail.com>
2014-12-08 17:40:46 +0100

    reviews with users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,29 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
-
-  respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
-
+  before_action :authenticate_user!
+  before_action :set_review, only: [:edit, :update, :destroy]
+ 
+  # GET /reviews/new
   def new
     @review = Review.new
-    respond_with(@review)
   end
 
+  # GET /reviews/1/edit
   def edit
   end
 
-  def create
+
+    def create
     @review = Review.new(review_params)
-    @review.save
-    respond_with(@review)
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to root_path, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208150306) do
+ActiveRecord::Schema.define(version: 20141208150639) do
 
   create_table "profils", force: true do |t|
     t.string   "prenom"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141208150306) do
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
